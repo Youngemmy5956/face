8e228f21508e2715b7179a45116946ec682c5369
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-06-19 16:58:51 +0100

    updated

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 
 export default function Form() {
-    const socket = io();
-    $('form').submit(() =>{
-      socket.emit('message', $('#m').val());
-      console.log('form')
-      $('#m').val('');
-      return false;
-    });
-    socket.on('message', (msg) =>{
-      $('#messages').append($('<li>').text(msg));
-    });
+   
 
 
   return (
